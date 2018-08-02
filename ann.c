0f06a10159f1082940c4bc64217db3f3b93e6121
moxotter <pitchnat@gmail.com>
2018-08-01 23:17:16 -0700

    Added network layer structure

--- ann.c
+++ ann.c
@@ -6,7 +6,6 @@ Basic artifical neural network to perform XOR function
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /* Activation Function
 Name:   Soft step (sigmoid)
@@ -29,7 +28,7 @@ double softplus(double x)
 }
 
 /*
-Generates random values from the standard normal distribution using Mox-Muller
+Generates random values from the standard normal distribution using Box-Muller
 transform
 */
 double rand_norm()
@@ -45,8 +44,7 @@ double rand_norm()
   return z0;
 }
 
-/*
-Initializes an array with random values from the standard normal distribution
+/* Initialize Random Standard Normal Distribution
 */
 void init_rand_norm(double *p_array, int size)
 {
@@ -55,17 +53,64 @@ void init_rand_norm(double *p_array, int size)
   }
 }
 
+/* Artificial Neural Network Layer
+Defines number of inputs and outputs
+Number of outputs is equal to number of nodes
+Defines biases and weights
+Number of biases is equal to number of nodes
+Number of weights is equal to number of inputs multiplied by number of outputs
+*/
+struct t_layer {
+  int n_in;
+  int n_out;
+
+  double *p_weights;  // size of in * out
+  double *p_biases;   // size of out
+};
+
+/* Initialize Zero
+*/
+void init_zero(double *p_array, int size)
+{
+  for (int i = 0; i < size; i++) {
+    p_array[i] = 0;
+  }
+}
+
+/* Initialize Artificial Neural Network Layer
+Initrializes an artificial neural network layer with random standard normal
+distribution weights and zero biases.
+*/
+void init_layer(struct t_layer *p_layer, int n_in, int n_out)
+{
+  p_layer->n_in = n_in;
+  p_layer->n_out = n_out;
+
+  double weights[n_in][n_out];
+  double *p_weights = &weights[0][0];
+  init_rand_norm(p_weights, n_in * n_out);
+  p_layer->p_weights = p_weights;
+
+  double biases[n_out];
+  double *p_biases = &biases[0];
+  init_zero(p_biases, n_out);
+  p_layer->p_biases = p_biases;
+}
+
 int main(int argc, char **argv)
 {
-  double array[3][3];
+  int n_in = 2;
+  int n_out = 3;
+
+  struct t_layer layer;
+  init_layer(&layer, n_in, n_out);
+
+  double array[n_in][n_out];
   double *p_array = &array[0][0];
+  init_rand_norm(p_array, n_in * n_out);
 
-  init_rand_norm(p_array, 9);
-
-  for (int row = 0; row < 3; row++)
-  {
-    for (int col = 0; col < 3; col++)
-    {
+  for (int row = 0; row < n_in; row++) {
+    for (int col = 0; col < n_out; col++) {
       printf("%.3f\t", array[row][col]);
     }
     printf("\n");
