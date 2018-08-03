85f072771c8fcf32c09a6e2c5a5b5eb02ced6b97
moxotter <pitchnat@gmail.com>
2018-08-02 19:00:57 -0700

    Implemented ann_layer using gsl

--- ann.c
+++ ann.c
@@ -6,14 +6,50 @@ Output layer: 1 node
 Target: Classify inputs as exclusive or
 */
 
-#include <gsl/gsl_math.h>
+#include <gsl/gsl_cblas.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_rng.h>
+#include <gsl/gsl_vector.h>
 
 #include <stdio.h>
 
 #include <sys/random.h>
+
+// artificial neural network layer
+typedef struct {
+  size_t size_in;
+  size_t size_out;
+  gsl_matrix *weights;
+  gsl_vector *biases;
+} ann_layer;
+
+/* allocate artificial neural network layer
+size_in : number of inputs
+size_out : number of outputs
+r : pointer to random number generator
+*/
+ann_layer *ann_layer_alloc(size_t size_in, size_t size_out, const gsl_rng *r)
+{
+  // allocate weights and biases
+  gsl_matrix *weights = gsl_matrix_alloc(size_in, size_out);
+  gsl_vector *biases = gsl_vector_alloc(size_out);
+
+// initialize weights with standard normal distribution random numbers
+  for (size_t i = 0; i < size_in * size_out; i++)
+    weights->data[i] = gsl_ran_gaussian(r, 1);
+
+// initialize biases with zero
+  gsl_vector_set_zero(biases);
+
+  ann_layer *layer = malloc(sizeof(ann_layer));
+  layer->size_in = size_in;
+  layer->size_out = size_out;
+  layer->weights = weights;
+  layer->biases = biases;
+
+  return layer;
+}
 
 int main(int argc, char **argv)
 {
@@ -25,9 +61,14 @@ int main(int argc, char **argv)
   gsl_rng *r = gsl_rng_alloc(gsl_rng_mt19937);
   gsl_rng_set(r, s);
 
-  // generate random numbers with standard normal distribution
-  for (int i = 0; i < 10; i++)
-    printf("%f\n", gsl_ran_gaussian(r, 1));
+  ann_layer *layer = ann_layer_alloc(2, 3, r);
+
+  for (size_t row = 0; row < 2; row++) {
+    for (size_t col = 0; col < 3; col++)
+      printf("%f\t", gsl_matrix_get(layer->weights, row, col));
+
+    printf("\n");
+  }
 
   return 0;
 }
